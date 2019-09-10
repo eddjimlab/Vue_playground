bacb18ba29b9c7511a37be9cffefdf8ad6304301
Eduard <eddjimlab@gmail.com>
2019-09-10 18:46:29 +0300

    form control example, email, password, confir password

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,92 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="container">
+      <h1 class="display-2 text-center">Form control</h1>
+      <h2>Email</h2>
+      <form class="pt-3">
+        <div class="form-group">
+          <label for="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            class="form-control"
+            :class="{ 'is-invalid': $v.email.$error }"
+            @blur="$v.email.$touch()"
+            v-model="email"
+          />
+          <div class="invalid-feedback" v-if="!$v.email.required">
+            The fields must not be empty!!!
+          </div>
+          <div class="invalid-feedback" v-if="!$v.email.email">
+            Email required!!!
+          </div>
+        </div>
+      </form>
+      <h2>Password</h2>
+      <form class="pt-3">
+        <div class="form-group">
+          <label for="password">password</label>
+          <input
+            type="password"
+            id="password"
+            class="form-control"
+            :class="{ 'is-invalid': $v.password.$error }"
+            @blur="$v.password.$touch()"
+            v-model="password"
+          />
+          <div class="invalid-feedback" v-if="!$v.password.minlength">
+            Min password length is {{ $v.password.$params.minLength.min }}. Now
+            it`s {{ password.length }}
+          </div>
+          <pre></pre>
+        </div>
+      </form>
+
+      <h2>Confirm password</h2>
+      <form class="pt-3">
+        <div class="form-group">
+          <label for="confirm">Confirm password</label>
+          <input
+            type="password"
+            id="confirm"
+            class="form-control"
+            :class="{ 'is-invalid': $v.confirm.$error }"
+            @blur="$v.confirm.$touch()"
+            v-model="confirm"
+          />
+          <div class="invalid-feedback" v-if="!$v.sameAs">
+            Password is not the same
+          </div>
+          <pre></pre>
+        </div>
+      </form>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import { required, email, minLength, sameAs } from "vuelidate/lib/validators";
 
 export default {
-  name: "app",
-  components: {
-    HelloWorld
+  data() {
+    return {
+      email: "",
+      password: "",
+      confirm: ""
+    };
+  },
+  validations: {
+    email: {
+      required,
+      email
+    },
+    password: {
+      minLength: minLength(6)
+    },
+    confirm: {
+      sameAs: sameAs("password")
+    }
   }
 };
 </script>
@@ -21,8 +96,13 @@ export default {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  text-align: left;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+h2 {
+  color: blue;
+  font-size: 2rem;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from "vue";
+import Vuelidate from "vuelidate";
 import App from "./App.vue";
-
-Vue.config.productionTip = false;
+Vue.use(Vuelidate);
 
 new Vue({
   render: h => h(App)
