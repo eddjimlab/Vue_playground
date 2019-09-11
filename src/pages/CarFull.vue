1da23362754b58accb77593df6c9623fe23c1969
Eduard <eddjimlab@gmail.com>
2019-09-11 20:24:17 +0300

    create children links

---
+++ src/pages/CarFull.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h2>Car Name: ford</h2>
+    <h4>Car year: 2016</h4>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss"></style>
--- src/pages/car.vue
+++ src/pages/car.vue
@@ -1,7 +1,19 @@
 <template>
   <div>
     <h1>Car id {{ id }}</h1>
-    <button class="btn btn-sm btn-info" @click="goBackToCars">Back</button>
+    <button class="btn btn-sm btn-primary mb-2" @click="goBackToCars">
+      Back
+    </button>
+    <br />
+    <routerLink
+      class="btn btn btn-info mt-2"
+      tag="button"
+      :to="{ name: 'carFull', params: { id: id } }"
+      >More info
+    </routerLink>
+    <!-- <routerLink class="btn btn btn-info mt-2" tag="button" :to="'/car/' + id + '/full/'">More info</routerLink> -->
+    <hr />
+    <router-view></router-view>
   </div>
 </template>
 <script>
