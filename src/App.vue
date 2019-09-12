627d88f0f7c8ac6ce1a9f01567310b2d6ec2119e
Eduard <eddjimlab@gmail.com>
2019-09-12 20:30:04 +0300

    start json-server, vue-resource

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,8 @@
 <template>
   <div class="container">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <div class="collapse navbar-collapse">
-        <div class="navbar-nav">
-          <routerLink
-            tag="li"
-            class="nav-item"
-            to="/"
-            exact
-            active-class="active"
-            ><a class="nav-link">Home</a></routerLink
-          >
-          <routerLink tag="li" class="nav-item" to="/cars" active-class="active"
-            ><a class="nav-link">Cars</a></routerLink
-          >
-          <routerLink
-            tag="li"
-            class="nav-item"
-            to="/car/3"
-            active-class="active"
-            ><a class="nav-link">Car 3</a></routerLink
-          >
-          <routerLink
-            tag="li"
-            class="nav-item"
-            to="/car/4"
-            active-class="active"
-            ><a class="nav-link">Car 4</a></routerLink
-          >
-        </div>
-      </div>
-    </nav>
-    <router-view></router-view>
+    <h1>Hello</h1>
   </div>
 </template>
-
 <script>
 export default {};
 </script>
-
-<style lang="scss"></style>
--- src/main.js
+++ src/main.js
@@ -1,13 +1,9 @@
 import Vue from "vue";
-import Vuelidate from "vuelidate";
+import VueResource from "vue-resource";
 import App from "./App.vue";
-import VueRouter from "vue-router";
-import router from "./routes";
 
-Vue.use(VueRouter);
-Vue.use(Vuelidate);
+Vue.use(VueResource);
 
 new Vue({
-  render: h => h(App),
-  router
+  render: h => h(App)
 }).$mount("#app");
