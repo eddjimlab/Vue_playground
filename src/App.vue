5c07ac7e643bf13677ffeec0d313c1b438b36797
Eduard <eddjimlab@gmail.com>
2019-09-12 20:57:39 +0300

    create POST request

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,37 @@
 <template>
-  <div class="container">
-    <h1>Hello</h1>
+  <div class="container pt-2">
+    <div class="form-group">
+      <label for="name">Car name</label>
+      <input type="text" class="form-control" v-model.trim="carName" />
+    </div>
+
+    <div class="form-group">
+      <label for="name">Car year</label>
+      <input type="text" class="form-control" v-model.number="carYear" />
+    </div>
+    <button class="btn btn-success" @click="createCar">Create Car</button>
   </div>
 </template>
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      carName: "",
+      carYear: 2008
+    };
+  },
+  methods: {
+    createCar() {
+      const car = {
+        name: this.carName,
+        year: this.carYear
+      };
+      this.$http.post("http://localhost:3000/cars", car).then(response => {
+        return response.json().then(newCar => {
+          console.log(newCar);
+        });
+      });
+    }
+  }
+};
 </script>
