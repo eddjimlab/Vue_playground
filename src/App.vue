9161ea4a958aea9daf818df888eafdf7e0269ccf
Eduard <eddjimlab@gmail.com>
2019-09-12 22:38:52 +0300

    fix mistake in db.json

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,33 @@
   <div class="container pt-2">
     <div class="form-group">
       <label for="name">Car name</label>
-      <input type="text" class="form-control" v-model.trim="carName" />
+      <input
+        type="text"
+        id="name"
+        class="form-control"
+        v-model.trim="carName"
+      />
     </div>
 
     <div class="form-group">
       <label for="name">Car year</label>
-      <input type="text" class="form-control" v-model.number="carYear" />
+      <input
+        type="text"
+        id="year"
+        class="form-control"
+        v-model.number="carYear"
+      />
     </div>
     <button class="btn btn-success" @click="createCar">Create Car</button>
+    <button class="btn btn-primary" @click="loadCars">Load Cars</button>
+
+    <hr />
+    <ul class="list-group">
+      <li class="list-group-item" v-for="car of cars" :key="car.id">
+        <strong>{{ car.name }}</strong>
+        - {{ car.year }}
+      </li>
+    </ul>
   </div>
 </template>
 <script>
@@ -17,7 +36,8 @@ export default {
   data() {
     return {
       carName: "",
-      carYear: 2008
+      carYear: 2008,
+      cars: []
     };
   },
   methods: {
@@ -26,11 +46,24 @@ export default {
         name: this.carName,
         year: this.carYear
       };
-      this.$http.post("http://localhost:3000/cars", car).then(response => {
-        return response.json().then(newCar => {
+      this.$http
+        .post("http://localhost:3000/cars", car)
+        .then(response => {
+          return response.json();
+        })
+        .then(newCar => {
           console.log(newCar);
         });
-      });
+    },
+    loadCars() {
+      this.$http
+        .get("http://localhost:3000/cars")
+        .then(response => {
+          return response.json();
+        })
+        .then(cars => {
+          console.log(cars);
+        });
     }
   }
 };
