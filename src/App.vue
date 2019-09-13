92da2693e585e19b2c0ca8121b8b0bcfbd9450dd
Eduard <eddjimlab@gmail.com>
2019-09-13 15:45:07 +0300

    set up Vuex

--- src/App.vue
+++ src/App.vue
@@ -1,63 +1,23 @@
 <template>
-  <div class="container pt-2">
-    <div class="form-group">
-      <label for="name">Car name</label>
-      <input
-        type="text"
-        id="name"
-        class="form-control"
-        v-model.trim="carName"
-      />
-    </div>
-
-    <div class="form-group">
-      <label for="name">Car year</label>
-      <input
-        type="text"
-        id="year"
-        class="form-control"
-        v-model.number="carYear"
-      />
-    </div>
-    <button class="btn btn-success" @click="createCar">Create Car</button>
-    <button class="btn btn-primary" @click="loadCars">Load Cars</button>
-
-    <hr />
-    <ul class="list-group">
-      <li class="list-group-item" v-for="car of cars" :key="car.id">
-        <strong>{{ car.name }}</strong>
-        - {{ car.year }}
-      </li>
-    </ul>
+  <div class="container text-center pt-5">
+    <app-counter :counter="counter"></app-counter>
+    <app-actions @counterUpdated="counter += $event"></app-actions>
   </div>
 </template>
+
 <script>
+import Counter from "./Counter";
+import Actions from "./Actions";
+
 export default {
   data() {
     return {
-      carName: "",
-      carYear: 2008,
-      cars: [],
-      resource: null
+      counter: 0
     };
   },
-  methods: {
-    createCar() {
-      const car = {
-        name: this.carName,
-        year: this.carYear
-      };
-      this.resource.save({}, car);
-    },
-    loadCars() {
-      this.resource
-        .get()
-        .then(response => response.json())
-        .then(cars => (this.cars = cars));
-    }
-  },
-  created() {
-    this.resource = this.$resource("cars");
+  components: {
+    appCounter: Counter,
+    appActions: Actions
   }
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -1,15 +1,8 @@
 import Vue from "vue";
-import VueResource from "vue-resource";
 import App from "./App.vue";
-
-Vue.use(VueResource);
-
-Vue.http.options.root = "http://localhost:3000/";
-
-Vue.http.interceptors.push(request => {
-  request.headers.set("Auth", "Rand Token " + Math.random());
-});
+import store from "./store";
 
 new Vue({
-  render: h => h(App)
+  render: h => h(App),
+  store
 }).$mount("#app");
