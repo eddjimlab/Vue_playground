f7a9c351a28c3e67e6c111ae529043b3dc9fa14c
Eduard <eddjimlab@gmail.com>
2019-09-13 12:51:35 +0300

    interceptors use

--- src/main.js
+++ src/main.js
@@ -3,7 +3,12 @@ import VueResource from "vue-resource";
 import App from "./App.vue";
 
 Vue.use(VueResource);
+
 Vue.http.options.root = "http://localhost:3000/";
+
+Vue.http.interceptors.push(request => {
+  request.headers.set("Auth", "Rand Token " + Math.random());
+});
 
 new Vue({
   render: h => h(App)
