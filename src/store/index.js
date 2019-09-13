609fee4bf0bb1fbf82ac55ae1ee4d1383e55812c
Eduard <eddjimlab@gmail.com>
2019-09-13 22:51:39 +0300

    optimization with modules

--- src/store/index.js
+++ src/store/index.js
@@ -1,27 +1,11 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import counter from "./counter";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-    counter: 0
-  },
-  mutations: {
-    changeCounter(state, payload) {
-      state.counter += payload;
-    }
-  },
-  actions: {
-    asyncChangeCounter(context, payload) {
-      setTimeout(() => {
-        context.commit("changeCounter", payload.counterValue);
-      }, payload.timeoutDelay);
-    }
-  },
-  getters: {
-    computedCounter(state) {
-      return state.counter + 20;
-    }
+  modules: {
+    counter
   }
 });
